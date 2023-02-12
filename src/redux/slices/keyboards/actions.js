dabadfc4af716bbf2b261a3d73e175385fc6987b
TheMelbine <melbineyt@gmail.com>
2023-02-12 19:45:57 +0300

    switch to redux thunk api

--- src/redux/slices/keyboards/actions.js
+++ src/redux/slices/keyboards/actions.js
@@ -2,8 +2,7 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 import {getKeyboards} from "../../../api/keyboardAPI";
 
 export const fetchKeyboards = createAsyncThunk("keyboards/getAll", async(params) =>{
-    const response = await getKeyboards(params)
-    return response;
+    return getKeyboards(params)
 
 
 })
